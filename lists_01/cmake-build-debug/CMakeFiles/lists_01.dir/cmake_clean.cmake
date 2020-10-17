7abfd057a76b3b94dc93c6ea58f2821c04579fc5
Kisely Tamás <tkisely@MacBook-Pro.local>
2020-10-17 21:01:45 +0200

    Copied the example

---
+++ lists_01/cmake-build-debug/CMakeFiles/lists_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lists_01.dir/main.c.o"
+  "lists_01"
+  "lists_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lists_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
